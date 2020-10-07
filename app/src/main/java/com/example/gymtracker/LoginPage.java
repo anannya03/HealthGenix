6125cd20ad4d0444418fc6d0307cc7957024cca9
harika2050 <49785475+harika2050@users.noreply.github.com>
2020-10-07 09:30:29 +0530

    dashboard

--- app/src/main/java/com/example/gymtracker/LoginPage.java
+++ app/src/main/java/com/example/gymtracker/LoginPage.java
@@ -2,24 +2,23 @@ package com.example.gymtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity_main);
 
     }
     private Button loginbutton;
     public void enterDashboard(View view)
     {
         loginbutton=(Button)findViewById(R.id.loginbutton);
-        Intent intent= new Intent(MainActivity.this, Dashboard.class);
-        startActivity(intent);
+        //Intent intent= new Intent(MainActivity.this, Dashboard.class);
+        //startActivity(intent);
     }
 }
