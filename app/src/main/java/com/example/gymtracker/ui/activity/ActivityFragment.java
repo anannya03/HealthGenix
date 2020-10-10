9f179a10082d38475715615376f8f7746a6dad32
harika2050 <49785475+harika2050@users.noreply.github.com>
2020-10-10 20:55:55 +0530

    added browse tab

--- app/src/main/java/com/example/gymtracker/ui/activity/ActivityFragment.java
+++ app/src/main/java/com/example/gymtracker/ui/activity/ActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.gymtracker.ui.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
