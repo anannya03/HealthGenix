a470b3705c7099bf524157abb928a53a316bcc61
harika2050 <49785475+harika2050@users.noreply.github.com>
2020-10-11 18:12:46 +0530

    added Activity fragment

--- app/src/main/java/com/example/gymtracker/ui/activity/ActivityFragment.java
+++ app/src/main/java/com/example/gymtracker/ui/activity/ActivityFragment.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -30,16 +31,7 @@ public class ActivityFragment extends Fragment {
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        activityViewModel =
-                ViewModelProviders.of(this).get(ActivityViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_activity, container, false);
-        final TextView textView = root.findViewById(R.id.text_activity);
-        activityViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        View view = inflater.inflate(R.layout.fragment_activity, container, false);
+            return view;
     }
 }
