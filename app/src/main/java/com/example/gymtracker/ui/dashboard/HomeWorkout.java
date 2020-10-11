d32165253220e3c1f46673c557304ae90367298a
harika2050 <49785475+harika2050@users.noreply.github.com>
2020-10-11 14:49:27 +0530

    made workout tab

---
+++ app/src/main/java/com/example/gymtracker/ui/dashboard/HomeWorkout.java
@@ -0,0 +1,16 @@
+package com.example.gymtracker.ui.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gymtracker.R;
+
+public class HomeWorkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_workout);
+    }
+}
--- app/src/main/java/com/example/gymtracker/ui/dashboard/tabfrag3.java
+++ app/src/main/java/com/example/gymtracker/ui/dashboard/tabfrag3.java
@@ -1,19 +1,41 @@
 package com.example.gymtracker.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.gymtracker.R;
 
 public class tabfrag3 extends Fragment {
+    Button homeworkout, bookworkout;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab3, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab3, container, false);
+        homeworkout=(Button)view.findViewById(R.id.homeworkout);
+        bookworkout=(Button)view.findViewById(R.id.bookworkout);
+        homeworkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getActivity(), HomeWorkout.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        return (view);
     }
 }
