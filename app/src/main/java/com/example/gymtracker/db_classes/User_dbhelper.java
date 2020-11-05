f621142878e86d7893bf16b0d0fff7788d07a8df
harika2050 <49785475+harika2050@users.noreply.github.com>
2020-11-05 22:46:41 +0530

    added workout_dbhelper

--- app/src/main/java/com/example/gymtracker/db_classes/User_dbhelper.java
+++ app/src/main/java/com/example/gymtracker/db_classes/User_dbhelper.java
@@ -18,7 +18,7 @@ public class User_dbhelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table User"+"(user_id integer primary key, fname text, lname text, email text, pwd text, age integer, gender char, phno text, weight decimal, height decimal, bmi decimal, mem_start date, mem_end date, gym_id integer, foreign key"+"(gym_id)"+" references gym_branches"+"(gym_id))");
+        db.execSQL("create table User"+"(user_id integer primary key, fname text, lname text, email text, pwd text, age integer, gender char, phno text, weight decimal, height decimal, bmi decimal, mem_start date, mem_end date, gym_id integer, foreign key(gym_id) references gym_branches(gym_id))");
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
