2f5c8c0748c8ad87308eeb6400cf63a5a548124c
harika2050 <49785475+harika2050@users.noreply.github.com>
2020-11-06 16:15:56 +0530

    added workout_dbhelper

--- app/src/main/java/com/example/gymtracker/db_classes/User_dbhelper.java
+++ app/src/main/java/com/example/gymtracker/db_classes/User_dbhelper.java
@@ -5,16 +5,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
-
 public class User_dbhelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME="GymTracker.db";
     public static final int DATABASE_VERSION=1;
     public int user_id=101101;
-
     User_dbhelper(Context context)
     {
         super(context, DATABASE_NAME, null,DATABASE_VERSION );
-
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
